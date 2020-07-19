9622453cfe15fb682d9605096fe1d04ac8ca448a
x300n <houseofxeon@gmail.com>
2020-07-19 21:10:13 +0200

    Implemented Security Config

---
+++ src/main/java/org/ahmedgaber/springsecurityjwt/controller/HelloResource.java
@@ -0,0 +1,13 @@
+package org.ahmedgaber.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
